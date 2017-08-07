64cc6ae84ecc29be423cf3bd5d53d389b24da437
actiivs <actiivs@gmail.com>
2017-08-08 02:30:42 +0800

    Update MSTest packages & add VideoCoverTest

---
+++ VideoTest/Cover/VideoCoverTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharingWorker.Video;
+
+namespace VideoTest.Cover
+{
+    [TestClass, TestCategory("Video Cover")]
+    public class VideoCoverTest
+    {
+        [TestMethod]
+        public async Task TestDmm()
+        {
+            var url = await VideoCover.QueryDmmImage("MIAE057");
+            Trace.WriteLine(url);
+            Assert.IsTrue(!string.IsNullOrEmpty(url));
+        }
+    }
+}
