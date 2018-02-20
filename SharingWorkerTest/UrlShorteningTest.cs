b1f3d9011d08d190c6630308f6cfa3332053bbe4
actiivs <actiivs@gmail.com>
2018-02-20 12:46:16 +0800

    Remove oke.io

--- SharingWorkerTest/UrlShorteningTest.cs
+++ SharingWorkerTest/UrlShorteningTest.cs
@@ -7,12 +7,6 @@ namespace SharingWorkerTest
     [TestClass, TestCategory("Url Shortening")]
     public class UrlShorteningTest
     {
-        [TestMethod]
-        public async Task TestOkeIo()
-        {
-            var shortener = new OkeIo();
-            var result = await shortener.GetLink("https://www.blogger.com");
-            Assert.IsTrue(!string.IsNullOrEmpty(result));
-        }
+
     }
 }
